b91a757a93d6d0cbc5d39a6b9b4c22fcfd43b895
carlitoslomon <calmos2266@gmail.com>
2022-08-28 01:31:53 -0600

    added the plane to the figure

--- frontend-hub/threejs/project1/project_one.js
+++ frontend-hub/threejs/project1/project_one.js
@@ -1,7 +1,12 @@
 function init(colorin, elementid) {
     var box = mkBox(2,2,2);
+    var plane = mkPlane();
     var scene = new THREE.Scene();
     scene.add(box);
+    scene.add(plane);
+    plane.rotation.x = Math.PI/2;
+    //this ensures that half of the box is not below the plane 
+    box.position.y = box.geometry.parameters.height/2;
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
     camera.position.x = 1;
 	camera.position.y = 2;
@@ -23,6 +28,19 @@ function init(colorin, elementid) {
         return mesh;
 
     }
+    //this is teh function that constructs a plane 
+  //a plane is simply another mesh
+  function mkPlane(){
+    var geometry = new THREE.PlaneGeometry(5,5);
+    var material = new THREE.MeshBasicMaterial({
+      color: "#ffffff",
+      side: THREE.DoubleSide
+    });
+
+    var mesh = new THREE.Mesh(geometry, material);
+
+    return mesh;
+  }
     
 
 }
